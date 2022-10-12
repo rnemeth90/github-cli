3b4b5596808f359c3d4e452dbacbe779e3bcd126
Ryan Nemeth <ryannemeth@live.com>
2022-10-12 10:40:44 -0400

    updates

--- main.go
+++ main.go
@@ -14,17 +14,27 @@ import (
 const apiBaseUrl string = `https://api.github.com/repos/`
 
 var (
-	token = flag.String("token", "", "github auth token")
-	owner = flag.String("owner", "", "the repo owner")
-	repo  = flag.String("repo", "", "github repo")
-	title = flag.String("title", "", "title for new issue")
-	body  = flag.String("body", "", "body for new issue")
+	token       = flag.String("token", "", "github auth token")
+	owner       = flag.String("owner", "", "the repo owner")
+	repoName    = flag.String("repoName", "", "github repo")
+	title       = flag.String("title", "", "title for new issue")
+	body        = flag.String("body", "", "body for new issue")
+	createRepo  = flag.Bool("createRepo", false, "Create a repo")
+	isPrivate   = flag.Bool("isPrivate", false, "Is the new repo private?")
+	description = flag.String("description", "", "Description for the repo")
+	help        = flag.Bool("help", false, "Print help")
 )
 
 // NewIssue - specify data fields for new github issue submission
 type NewIssue struct {
 	Title string `json:"title"`
 	Body  string `json:"body"`
+}
+
+type Repository struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Private     bool   `json:"private"`
 }
 
 func usage() {
@@ -45,8 +55,14 @@ func main() {
 		os.Exit(2)
 	}
 
-	// createIssue(*repo, *owner, *title, *body, *token)
-	createRepo(*owner, *repo)
+	if *help {
+		flag.PrintDefaults()
+		return
+	}
+
+	if *createRepo {
+		makeRepo(*repoName, *description, *token, *isPrivate)
+	}
 }
 
 func createIssue(repo, owner, title, body, token string) {
@@ -75,6 +91,47 @@ func createIssue(repo, owner, title, body, token string) {
 }
 
 //createRepo
-func createRepo(owner, name string) {
-	fmt.Printf("Creating repo: %s/%s\n", owner, name)
+func makeRepo(name, description, token string, private bool) error {
+	url := "https://api.github.com/user/repos"
+
+	if name == "" || token == "" {
+		fmt.Println("You must specify a repo name and token")
+		os.Exit(2)
+	}
+
+	fmt.Printf("Creating repo: %s\n", name)
+
+	repoData := Repository{
+		Name:        name,
+		Description: description,
+		Private:     private,
+	}
+
+	jsonData, err := json.Marshal(repoData)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonData))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Authorization", "Bearer "+token)
+	req.Header.Set("Accept", "application/vnd.github+json")
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// we should return an error from this method, not this:
+	if resp.StatusCode != http.StatusCreated {
+		fmt.Printf("Response code is %d\n", resp.StatusCode)
+		body, _ := ioutil.ReadAll(resp.Body)
+		//print body as it may contain hints in case of errors
+		fmt.Println(string(body))
+		log.Fatal(err)
+	}
+	return nil
 }
